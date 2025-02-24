dabbfc1910bbd1c32df1828f33f306193bf5c040
Ronak Mathur <ronakmathur9@gmail.com>
2025-02-24 12:09:25 +0530

    in-memory caching implements for search term

--- vanilla-js-autocomplete/src/js/main.js
+++ vanilla-js-autocomplete/src/js/main.js
@@ -1,24 +1,38 @@
 const searchProduct = document.getElementById("search");
 const matchItems = document.getElementById("match-items");
+const cacheRes = {};
+
+const cacheSearchQuery = function (searchCache, data) {
+  if (cacheRes[searchCache]) {
+    console.log("api cache return", searchCache, data);
+    return cacheRes[searchCache];
+  }
+  cacheRes[searchCache] = data;
+  console.log("cached data", cacheRes);
+};
 
 const searchPeople = async (search) => {
-  const res = await fetch("../../data/data.json");
-  const products = await res.json();
-  //get all the matched names from api
-  let getMatches = products.filter((product) => {
-    const regex = new RegExp(`^${search}`, "gi");
-    return (
-      product.name.toLowerCase().match(regex) ||
-      product.description.toLowerCase().match(regex)
-    );
-  });
-
-  if (search.length === 0) {
-    getMatches = []; // don't show anything if nothing is typed or cleared
-    matchItems.innerHTML = "";
+  try {
+    console.log("api call ", search)
+    const res = await fetch("../../data/data.json");
+    const products = await res.json();
+    //get all the matched names from api
+    let getMatches = products.filter((product) => {
+      const regex = new RegExp(`^${search}`, "gi");
+      return (
+        product.name.toLowerCase().match(regex) ||
+        product.description.toLowerCase().match(regex)
+      );
+    });
+    if (search.length === 0) {
+      getMatches = []; // don't show anything if nothing is typed or cleared
+      matchItems.innerHTML = "";
+    }
+    cacheSearchQuery(search, getMatches);
+    showPeopleDetails(getMatches);
+  } catch (error) {
+    throw new Error("Error fetching data from the server...");
   }
-
-  showPeopleDetails(getMatches);
 };
 
 const debounce = (fn, delay) => {
@@ -51,7 +65,7 @@ const showPeopleDetails = (matched) => {
 };
 
 // Create the debounced version of searchProductsItems outside of the event listener.
-const debouncedSearch = debounce(searchPeople, 500);
+const debouncedSearch = debounce(searchPeople, 400);
 searchProduct.addEventListener("input", () =>
-    debouncedSearch(searchProduct.value)
+  debouncedSearch(searchProduct.value)
 );

4cf69565b782bb5ee60de7848306017ec102252a
Ronak Mathur <ronakmathur9@gmail.com>
2025-03-01 08:32:41 +0530

    drag and drop application

--- drag-and-drop-vanilla-js/src/main.js
+++ drag-and-drop-vanilla-js/src/main.js
@@ -1,55 +1,82 @@
-console.log("draggable coomponent");
-
-const draggableCandy = document.querySelector(".candy");
 const draggableArea = document.querySelectorAll(".drag-area");
+const imageShow = document.querySelector("#img-show");
+const dragUploadFile = document.getElementById("file-upload");
+const drag_img = document.querySelector(".drag-img");
 
 // drag and drap events
-
 let isDragged; // store the dragged elements
 
-// drag event listener fires
-draggableCandy.addEventListener("drag", handleOnDragItem);
-
-// on drag start event listener fires
-draggableCandy.addEventListener("dragstart", handleOnDragStart);
+dragUploadFile.addEventListener("change", (e) => {
+  displayMultipleImages(e.target.files);
+});
 
 //function to handle drag start
-function handleOnDragStart(e) {
-  draggableArea.forEach((area) => {
-    area.addEventListener("dragover", onDragOver);
-
-    area.addEventListener("dragenter", onDragEnter);
-    area.addEventListener("dragleave", onDragLeave);
-    area.addEventListener("drop", onDragDrop);
-    area.addEventListener("dragend", onDragEnd);
-  });
-  isDragged = e.target;
-}
+draggableArea.forEach((area) => {
+  area.addEventListener("dragover", onDragOver);
+  area.addEventListener("dragenter", onDragEnter);
+  area.addEventListener("dragleave", onDragLeave);
+  area.addEventListener("drop", onDragDrop);
+  area.addEventListener("dragend", onDragEnd);
+});
 
 function handleOnDragItem(e) {
   isDragged = e.target;
 }
 
 function onDragOver(e) {
+  e.stopPropagation();
   e.preventDefault();
-  e.target.classList.add("currentBox")
+  e.target.classList.add("currentBox");
 }
 
 function onDragLeave(e) {
-  e.target.classList.remove("currentBox")
+  e.target.classList.remove("currentBox");
 }
 
 function onDragDrop(e) {
-  isDragged = e.target.append(isDragged);
-  e.target.classList.remove("currentBox")
+  e.preventDefault();
+  e.target.classList.remove("currentBox");
+  const dropTarget = e.target.closest('.drag-area');
+  if (!dropTarget) return;
+  
+  if (isDragged && isDragged.tagName === 'IMG') {
+    if (dropTarget !== isDragged.parentElement) {
+      dropTarget.appendChild(isDragged);
+    }
+  }
 }
 
 function onDragEnter(e) {
-  e.target.classList.add("currentBox")
+  e.target.classList.add("currentBox");
 }
 
 function onDragEnd(e) {
   setTimeout(() => {
-    e.target.classList.remove("currentBox")
-  }, 100)
+    e.target.classList.add("hoverLeave");
+  }, 100);
+}
+
+// handling multiple files
+function displayMultipleImages(files) {
+  for (let file of files) {
+    if (!file && !file.type("image/*")) {
+      return false;
+    }
+  }
+
+  for (let file of files) {
+    let reader = new FileReader();
+    reader.onload = () => {
+      let image = new Image();
+      image.src = reader.result;
+      image.style.display = "block";
+      image.className = "drag-img";
+      image.setAttribute('draggable', 'true');
+      image.addEventListener('dragstart', function(e) {
+        isDragged = e.target;
+      });
+      imageShow.appendChild(image);
+    };
+    reader.readAsDataURL(file);
+  }
 }
